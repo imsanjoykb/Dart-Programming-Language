94556700fb6e05fd4b853fbc97ae051d1a628459
imsanjoykb <sanjoy.eee32@gmail.com>
2021-07-12 17:58:42 +0600

    Variables File Added

---
+++ L-1 Variables.dart
@@ -0,0 +1,42 @@
+/*
+Types of Variable In Dart
+1. Integer
+2. Double
+3. String
+4. Booleans
+5. Lists
+6. Maps
+*/
+void main() {
+  //Declear Int Variable
+  int var1 = 120;
+  print(var1);
+
+  //Declear double
+  double var2 = 1848.33033;
+  print(var2);
+
+  //Declear String
+  String var3 = 'This is all about Dart thing';
+  print(var3);
+
+  //Declear booleans
+  bool var4 = true;
+  print(var4);
+
+  //Declear Lists
+  var list = [1, 2, 3];
+  var abc = [4, 6, 7, 8];
+  print(list);
+  print(abc);
+
+  //Declear Maps
+  var stuLife = {'Place': 'School', 'Study': 'Book', 'ForMan': 'Student'};
+  print(stuLife);
+
+  //Declear Runes
+  var heart_symbol = '\u2665';
+  var laugh_symbol = '\u{1f600}';
+  print(heart_symbol);
+  print(laugh_symbol);
+}
