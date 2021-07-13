564b43855298318d42eee1219fe746049f38a854
imsanjoykb <sanjoy.eee32@gmail.com>
2021-07-13 10:13:59 +0600

    Queues File Added

---
+++ L-7 Queues.dart
@@ -0,0 +1,19 @@
+import 'dart:collection';
+
+void main() {
+// Creating a Queue
+  Queue<String> geek = new Queue<String>();
+
+// Printing default
+// value of queue
+  print(geek);
+
+// Adding elements in a Queue
+  geek.add("Geeks");
+  geek.add("For");
+  geek.add("Geeks");
+
+// Printing the
+// inserted elements
+  print(geek);
+}
